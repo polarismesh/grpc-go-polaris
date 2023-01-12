706f99d98fd1e05dfb7d17954eba085c5397c1bc
chuntaojun <liaochuntao@live.com>
2023-01-12 19:27:07 +0800

    refactor:优化grpc-go-polaris相关实现

--- examples/quickstart/consumer/main.go
+++ examples/quickstart/consumer/main.go
@@ -71,9 +71,9 @@ func main() {
 		ctx := metadata.NewIncomingContext(context.Background(), metadata.MD{})
 		ctx = metadata.AppendToOutgoingContext(ctx, "uid", r.Header.Get("uid"))
 
-		//请求时设置本次请求的负载均衡算法
-		//ctx = polaris.SetLbPolicy(ctx, api.LBPolicyRingHash)
-		//ctx = polaris.SetLbHashKey(ctx, r.Header.Get("uid"))
+		// 请求时设置本次请求的负载均衡算法
+		// ctx = polaris.RequestScopeLbPolicy(ctx, api.LBPolicyRingHash)
+		// ctx = polaris.RequestScopeLbHashKey(ctx, r.Header.Get("uid"))
 		resp, err := echoClient.Echo(ctx, &pb.EchoRequest{Value: value})
 		log.Printf("send message, resp (%v), err(%v)", resp, err)
 		if nil != err {
